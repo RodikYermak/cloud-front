974af0f085e35ca3e209a5bb19c1e46ef5865c83
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-18 11:40:17 -0700

    login form ui from antd library

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,6 +1,54 @@
 import React from 'react';
 import styles from './Auth.module.scss';
+import { Button, Form, Input } from 'antd';
 
 export const LoginForm: React.FC = () => {
-    return <div className={styles.root}>Форма авторизации</div>;
+    const onSubmit = (values: any) => {
+        console.log(values);
+    };
+    return (
+        <div className={styles.formBlock}>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                onFinish={onSubmit}>
+                <Form.Item
+                    label="E-mail"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Укажите почту',
+                        },
+                    ]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Пароль"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Укажите пароль',
+                        },
+                    ]}>
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}>
+                    <Button type="primary" htmlType="submit">
+                        {' '}
+                        Войти
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 };
